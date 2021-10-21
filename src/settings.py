856bef6520c6c0c59946c85749992d8d703db294
mehuldhokia <mehuldhokia@yahoo.com>
2021-10-21 09:23:00 +0530

    4 commit

--- src/settings.py
+++ src/settings.py
@@ -133,8 +133,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
